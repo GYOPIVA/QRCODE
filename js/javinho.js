51b3ef29895cedc4de80ce8d5dc9cd7a6a3e70ad
GYOPIVA <boy3giovane@gmail.com>
2025-05-13 08:51:40 -0300

    Add files via upload

---
+++ js/javinho.js
@@ -0,0 +1,42 @@
+let btnGerar = document.querySelector("#gerar-qr")
+let box = document.querySelector(".box")
+let divQrCode = document.querySelector(".qr-code")
+
+
+function gerarCodigoQr() {
+
+    let linkInput = document.querySelector("#campo-qr").value; /* pegando valor digitado no input para variavel */
+
+    box.innerHTML= ""
+
+    if(linkInput.trim() !=""){
+        
+        var qrcode = new QRCode(box, {
+            text: linkInput,
+            width: 300,
+            height: 300,
+        });
+
+        setTimeout(() => {
+            
+            const img = box.querySelector("img")
+            const download = document.querySelector("#download")
+        
+            download.href = img.src
+            
+        }, 300);
+
+    divQrCode.classList.add("display-hidden")
+
+
+
+
+    }
+    else{
+        divQrCode.classList.remove("display-hidden")
+        alert("Campo vazio!")
+    }
+}
+
+btnGerar.addEventListener("click", gerarCodigoQr)
+
